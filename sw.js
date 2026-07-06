ae15a7a89ab985e278f76a117a90c3b2bdffa927
revealimpact-afk <reveal.impact@gmail.com>
2026-07-06 12:55:58 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="mri-resa-v1";
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{ if(e.request.method!=="GET")return; e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return r;}).catch(()=>caches.match(e.request))); });
